a3df71ca4a416304c2d8e0b3385098dc859bee65
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-06-17 09:43:20 +0100

    db updated

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.PORT || 4001; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log("server working on port: ", port);
+});
